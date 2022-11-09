7b91f62116402453fdcfd47e2baf9c3ef234ab9c
NixonVuele <userTest1@CITTE-CCCEL-057.utpl.edu.ec>
2022-11-08 20:35:30 -0500

    tare2

---
+++ pets/tarea2/internas/procesar.php
@@ -0,0 +1,8 @@
+<?php
+    $nombres = $_POST['nombres'];
+    $nombres = $_POST['apellidos'];
+    $nombres = $_POST['telefono'];
+    echo"<strong>".$nombres." ".$apellidos;
+    echo "<hr>";
+    echo $telefono;
+?>
